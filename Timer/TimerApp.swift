64c6efdac9ad903e1862dac3a74c62354b9895f5
Daigo Shitara <stain.witness+github@gmail.com>
2021-06-19 15:29:28 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by shitara on 2021/06/19.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
